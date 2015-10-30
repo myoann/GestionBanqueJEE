6e01147ecf05e8dbd1dd3a2b607d95786b215d3c
Florian <florian.m.06@gmail.com>
2015-10-30 08:52:10 +0100

    Nouveau template pour la page de connexion

--- ProjetBanqueMassaMoise-war/src/java/managedbeans/ConnexionMBean.java
+++ ProjetBanqueMassaMoise-war/src/java/managedbeans/ConnexionMBean.java
@@ -96,7 +96,7 @@ public class ConnexionMBean implements Serializable {
         this.mdpCourant ="";
         this.loginUtilisateurCourant="";
         try {
-            FacesContext.getCurrentInstance().getExternalContext().redirect("faces/login.xhtml");
+            FacesContext.getCurrentInstance().getExternalContext().redirect("login.xhtml");
         } catch (IOException ex) {
             Logger.getLogger(CompteBancaireMBean.class.getName()).log(Level.SEVERE, null, ex);
         }
